10439b68fa6cb9c030a7c041184ea550b1baf240
Michael <michaelthomas0721@icloud.com>
2022-02-26 17:53:47 -0500

    27th problem

--- 3sum - Java/Solution.java
+++ 3sum - Java/Solution.java
@@ -1,9 +1,37 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 class Solution {
     public static void main(String[] args) {
         int[] nums = { -1, 0, 1, 2, -1, -4 };
+        System.out.println(threeSum(nums));
     }
 
-    public List<List<Integer>> threeSum(int[] nums) {
-
+    public static List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i + 2 < nums.length; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int j = i + 1, k = nums.length - 1;
+            while (j < k) {
+                if (nums[j] + nums[k] == -nums[i]) {
+                    ans.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
+                    k--;
+                    while (j < k && nums[j] == nums[j - 1])
+                        j++;
+                    while (j < k && nums[k] == nums[k + 1])
+                        k--;
+                } else if (nums[j] + nums[k] > -nums[i]) {
+                    k--;
+                } else {
+                    j++;
+                }
+            }
+        }
+        return ans;
     }
 }
---
+++ Shortest Path Visiting All Nodes - Java/Solution.java
@@ -0,0 +1,9 @@
+class Solution {
+    public static void main(String[] args) {
+        int[][] graph = { { 1 }, { 0, 2, 4 }, { 1, 3, 4 }, { 2 }, { 1, 2 } };
+    }
+
+    public static int shortestPathLength(int[][] graph) {
+
+    }
+}

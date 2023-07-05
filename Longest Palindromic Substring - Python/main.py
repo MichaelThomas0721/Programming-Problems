97eff7adf90c570359cf48cff4959b43b4315421
MichaelThomas0721 <michaelthomas0721@icloud.com>
2023-07-05 15:31:30 +0000

    Version 3 of Longest Palindromic Substring

--- Longest Palindromic Substring - Python/main.py
+++ Longest Palindromic Substring - Python/main.py
@@ -1,30 +1,34 @@
-# Version 2.1 completed, accepted by LeetCode. Turns out I left a print statement in that slowed it down and used a lot more memory. Memory usage has been greatly improved.
+# Version 3 completed, accepted by LeetCode. Made it so it doesn't loop over duplicate letters, thought it might save some time but didn't do much.
 def findSame(s, length, sc, idx, dir):
     while (idx >= 0 and idx < length and s[idx] == sc):
         idx += dir
     return idx
 def findPalindrome(s, middle, length):
     if middle == 0:
-        return s[0]
+        return [s[0], []]
     elif middle == length - 1:
-        return s[length - 1]
+        return [s[length - 1], []]
     sc = s[middle]
     leftIdx = middle - 1
     rightIdx = middle + 1
     if leftIdx >= 0:
         leftIdx = findSame(s, length, sc, leftIdx, -1)
+
     else:
         leftIdx += 1
     if rightIdx < length:
         rightIdx = findSame(s, length, sc, rightIdx, 1)
     else:
         rightIdx -= 1
+    sames = []
+    if (leftIdx - rightIdx > 3):
+        sames = [leftIdx, rightIdx]
     while (leftIdx >= 0 and rightIdx < length):
         if (s[leftIdx] != s[rightIdx]):
             break
         leftIdx -= 1
         rightIdx += 1
-    return s[leftIdx + 1:rightIdx]
+    return [s[leftIdx + 1:rightIdx], sames]
          
 
 
@@ -48,6 +52,10 @@ class Solution:
         while leftIdx >= 0 and rightIdx < length:
             if left:
                 pal = findPalindrome(s, leftIdx, length)
+                if pal[1] != []:
+                    leftIdx = pal[1][0]
+                    rightIdx = pal[1][1]
+                pal = pal[0]
                 if (len(pal) > longestLen):
                     longest = pal
                     longestLen = len(pal)
@@ -55,6 +63,10 @@ class Solution:
                 left = False
             else:
                 pal = findPalindrome(s, rightIdx, length)
+                if pal[1] != []:
+                    leftIdx = pal[1][0]
+                    rightIdx = pal[1][1]
+                pal = pal[0]
                 if (len(pal) > longestLen):
                     longest = pal
                     longestLen = len(pal)
@@ -64,5 +76,5 @@ class Solution:
 
 
 
-    s = "aaaaam"
+    s = "adfaaaaafm"
     print(longestPalindrome(1, s))

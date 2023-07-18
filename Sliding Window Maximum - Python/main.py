7f33488dba47573c5b4462f8cc339aa825cd93bc
MichaelThomas0721 <michaelthomas0721@icloud.com>
2023-07-18 12:30:56 +0000

    Sliding Window Maximum

--- Sliding Window Maximum - Python/main.py
+++ Sliding Window Maximum - Python/main.py
@@ -1,33 +1,31 @@
-# Not my answer, just trying to understand it :)
+# Version 1. Works and is fast but kinda sucks on memory, not sure how I would do it fast and memory efficient at the moment so maybe I will come back another day.
 
 import collections
 
 class Solution:
     def maxSlidingWindow(self, nums, k: int):
-        output = []
-        q = collections.deque()  # index
-        l = r = 0
-        # O(n) O(n)
-        while r < len(nums):
-            # pop smaller values from q
-            while q and nums[q[-1]] < nums[r]:
-                q.pop()
-            q.append(r)
-
-            # remove left val from window
-            if l > q[0]:
-                q.popleft()
-
-            if (r + 1) >= k:
-                output.append(nums[q[0]])
+        stack = collections.deque()
+        l = 0
+        ans = []
+        for i, num in enumerate(nums):
+        # Remove the numbers on the right of the stack that are less than the current number
+            while stack and nums[stack[-1]] <= num:
+                stack.pop()
+            stack.append(i)
+        # If the left most number is less than the left pointer, remove it
+            if stack[0] < l:
+                stack.popleft()
+        # The max is the left most number
+            if i+1 >= k:
+                ans.append(nums[stack[0]])
                 l += 1
-            r += 1
 
-        return output
-
+        return ans
 
     nums = [1,3,-1,-3,5,3,6,7]
     k = 3
     # nums = [1]
+    # k = 1
+    # nums = [1,-1]
     # k = 1
     print(maxSlidingWindow(1, nums, k))
